729bc48d821d8c5ece72b5d3996b16e3d161a608
Jessica Schneider <jessica.schneider1207@gmail.com>
2017-01-24 20:43:19 -0500

    missed some stuff

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule DistributedTest.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     preferred_cli_env: [{:"test.distributed", :test}],
      description: description(),
      package: package(),
      deps: deps()]
